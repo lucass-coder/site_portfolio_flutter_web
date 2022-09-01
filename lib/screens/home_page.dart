ab9843e5dd2240ced0f00e4e6a097cf692444190
Lucas <lucas.santana@domvsit.com.br>
2022-08-31 21:50:00 -0300

    Resolvendo overflow do Menu Iniciar utilizando Stack

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -23,108 +23,127 @@ class _MyHomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff008081),
-      body: Row(
+      body: Stack(
         children: [
           Column(
+            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Image.asset(
-                  'assets/images/computer.png',
-                  width: 70,
-                  height: 70,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Image.asset(
-                  'assets/images/phone.png',
-                  width: 70,
-                  height: 70,
-                ),
-              ),
-              Spacer(),
               Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Stack(children: [
-                    isAtivo
-                        ? Container(
-                            width: 224,
-                            height: 400,
-                            decoration: BoxDecoration(
-                              color: const Color(0xffC0C7C8),
-                              border: Border.all(),
-                            ),
-                            child: Row(
-                              children: [
-                                Container(
-                                  height: 400,
-                                  width: 50,
-                                  decoration: const BoxDecoration(
-                                    color: Colors.grey,
-                                  ),
-                                  child: RotatedBox(
-                                    quarterTurns: 3,
-                                    child: Text(
-                                      'FlutterMan',
-                                    ),
-                                  ),
-                                ),
-                                Expanded(
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: [
-                                      SizedBox(height: 8),
-                                      OptionsStart(
-                                          icon: 'icon_programs',
-                                          name: 'Programs'),
-                                      SizedBox(height: 8),
-                                      OptionsStart(
-                                          icon: 'folder', name: 'Documents'),
-                                      SizedBox(height: 8),
-                                      OptionsStart(
-                                          icon: 'icon_programs',
-                                          name: 'Settings'),
-                                    ],
-                                  ),
-                                )
-                              ],
-                            ),
-                          )
-                        : Container(
-                            color: Colors.red,
-                          ),
-                  ]),
-                  Container(
-                    //color: Colors.grey[400],
-                    color: const Color(0xffC0C7C8),
-                    width: MediaQuery.of(context).size.width,
-                    height: 46,
-                    child: Row(
-                      children: [
-                        SizedBox(width: 20),
-                        GestureDetector(
-                          onTap: () => {
-                            setState(() {
-                              if (isAtivo == true) {
-                                isAtivo = false;
-                              } else {
-                                isAtivo = true;
-                              }
-                            })
-                          },
-                          child: BottomStart(isAtivo: isAtivo),
-                        ),
-                        Spacer(),
-                        Horario(),
-                        SizedBox(width: 16),
-                      ],
+                  Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Image.asset(
+                      'assets/images/computer.png',
+                      width: 70,
+                      height: 70,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Image.asset(
+                      'assets/images/phone.png',
+                      width: 70,
+                      height: 70,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Image.asset(
+                      'assets/images/phone.png',
+                      width: 70,
+                      height: 70,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Image.asset(
+                      'assets/images/phone.png',
+                      width: 70,
+                      height: 70,
                     ),
                   ),
                 ],
+              )
+            ],
+          ),
+          //Spacer(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              isAtivo
+                  ? Container(
+                      width: 224,
+                      height: 400,
+                      decoration: BoxDecoration(
+                        color: const Color(0xffC0C7C8),
+                        border: Border.all(),
+                      ),
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 400,
+                            width: 50,
+                            decoration: const BoxDecoration(
+                              color: Colors.grey,
+                            ),
+                            child: RotatedBox(
+                              quarterTurns: 3,
+                              child: Text(
+                                'FlutterMan',
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                SizedBox(height: 8),
+                                OptionsStart(
+                                    icon: 'icon_programs', name: 'Programs'),
+                                SizedBox(height: 8),
+                                OptionsStart(icon: 'folder', name: 'Documents'),
+                                SizedBox(height: 8),
+                                OptionsStart(
+                                    icon: 'icon_programs', name: 'Settings'),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  : Container(
+                      color: Colors.red,
+                    ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  //color: Colors.grey[400],
+                  color: const Color(0xffC0C7C8),
+                  width: MediaQuery.of(context).size.width,
+                  height: 46,
+                  child: Row(
+                    children: [
+                      SizedBox(width: 20),
+                      GestureDetector(
+                        onTap: () => {
+                          setState(() {
+                            if (isAtivo == true) {
+                              isAtivo = false;
+                            } else {
+                              isAtivo = true;
+                            }
+                          })
+                        },
+                        child: BottomStart(isAtivo: isAtivo),
+                      ),
+                      Spacer(),
+                      Horario(),
+                      SizedBox(width: 16),
+                    ],
+                  ),
+                ),
               ),
             ],
           ),
