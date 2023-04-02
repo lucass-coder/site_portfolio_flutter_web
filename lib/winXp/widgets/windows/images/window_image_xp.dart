7bd9aad92602741eaf8e61ac567c30618e23c25a
Lucas <santana.lucas@aluno.ifsp.edu.br>
2023-04-01 22:33:42 -0300

    Adicionando novos botões ao menu iniciar e testando webview

--- lib/winXp/widgets/windows/images/window_image_xp.dart
+++ lib/winXp/widgets/windows/images/window_image_xp.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_utils/src/extensions/internacionalization.dart';
-import 'package:site_portfolio/winXp/widgets/windows/images/image_box_xp.dart';
-import 'package:site_portfolio/winXp/widgets/windows/header/window_header.dart';
 
 class WindowImageXp extends StatelessWidget {
   const WindowImageXp({
