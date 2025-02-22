a231c4d3756d720663aac9c310447a397e8a7c61
Lucas <lsantana.raro@lidercap.com.br>
2025-02-22 19:04:21 -0300

    finalizacao de modularizacao

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:site_portfolio/games/lib/routes.dart';
+import 'package:game_portfolio_lucas/routes.dart';
 import 'package:get/get.dart';
 import 'package:site_portfolio/screens/home_page.dart';
 import 'package:site_portfolio/utils/translation_keys.dart';
